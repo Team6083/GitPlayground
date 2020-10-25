794d385abbb3f94d381eb38ea064c88ff01ab223
Anna0405 <anna20050405@gmail.com>
2020-10-25 15:59:34 +0800

    hello

--- fileSix.java
+++ fileSix.java
@@ -2,5 +2,7 @@ class App {
     public static void main() {
         System.out.println(1);
         // write something down
+        System.out.println("nothing");
+        System.out.println("trap");
     }
 } 
